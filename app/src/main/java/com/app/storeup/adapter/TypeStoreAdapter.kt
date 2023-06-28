83c0eab1bd1c953f311b76084a9e08548017a864
Alfonso <danidejesus47116@gmail.com>
2023-06-28 16:52:35 -0600

    Se agregaron imagenes para los tipos de tienda.

--- app/src/main/java/com/app/storeup/adapter/TypeStoreAdapter.kt
+++ app/src/main/java/com/app/storeup/adapter/TypeStoreAdapter.kt
@@ -3,9 +3,10 @@ package com.app.storeup.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.app.storeup.R
 import com.app.storeup.databinding.ItemTypeStoreBinding
 import com.app.storeup.model.entitys.CategoriasModel
 
@@ -13,6 +14,7 @@ class TypeStoreAdapter(private val c: Context) : RecyclerView.Adapter<TypeStoreA
     private var categorias: List<CategoriasModel> = emptyList()
 
     inner class ViewHolder(private val binding: ItemTypeStoreBinding) : RecyclerView.ViewHolder(binding.root) {
+        val iv: ImageView =binding.ivImageStore
         fun typeCategorias(categorias: CategoriasModel) {
             binding.tvTypeStore.text = categorias.tipo
         }
@@ -26,6 +28,35 @@ class TypeStoreAdapter(private val c: Context) : RecyclerView.Adapter<TypeStoreA
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val typeCategoria = categorias[position]
         holder.typeCategorias(typeCategoria)
+        when (position) {
+            0 -> {
+                holder.iv.setImageResource(R.drawable.stores)
+            }
+            1 -> {
+                holder.iv.setImageResource(R.drawable.abarrotes)
+            }
+            2 -> {
+                holder.iv.setImageResource(R.drawable.refaccionarias)
+            }
+            3->{
+                holder.iv.setImageResource(R.drawable.turismo)
+            }
+            4->{
+                holder.iv.setImageResource(R.drawable.tecnologia)
+            }
+            5->{
+                holder.iv.setImageResource(R.drawable.food)
+            }
+            6->{
+                holder.iv.setImageResource(R.drawable.sports)
+            }
+            7->{
+                holder.iv.setImageResource(R.drawable.beauty)
+            }
+            8->{
+                holder.iv.setImageResource(R.drawable.others)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
